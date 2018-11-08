46de995dc7815e65617dc9ed63e66638ddf4fc0d
kyang12 <kyang12@ncsu.edu>
2018-11-08 13:56:03 -0500

    add snippets template

--- src/main/java/org/ncsu/dnn/tf/CodeGenerator.java
+++ src/main/java/org/ncsu/dnn/tf/CodeGenerator.java
@@ -1,8 +1,13 @@
 package org.ncsu.dnn.tf;
 
 import java.io.*;
+import java.util.ResourceBundle;
 
 public class CodeGenerator {
+    static final ResourceBundle snippets;
+    static {
+        snippets = ResourceBundle.getBundle("org.ncsu.dnn.tf.template.snippets");
+    }
     private void appendFile(PrintStream out, File file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             for (String line = reader.readLine(); null != line; line = reader.readLine()) {
@@ -20,6 +25,8 @@ public class CodeGenerator {
         CodeGenerator codeGenerator = new CodeGenerator();
         PrintStream out = new PrintStream(new BufferedOutputStream(new FileOutputStream(test)));
         codeGenerator.appendFile(out, header);
+        out.printf(snippets.getString("layer.conv"), "\t\t", "\t\t", "\t\t");
+        out.println();
         codeGenerator.appendFile(out, footer);
         out.close();
     }
